ab5a783299a1ce746bb7d6c4c2cdc9b32f01981e
JefersonSg <jefersongabri@gmail.com>
2023-07-24 23:02:20 -0300

    Concluindo carrinho de compras

--- src/Components/carrinho/BagItens.jsx
+++ src/Components/carrinho/BagItens.jsx
@@ -4,6 +4,7 @@ import { color } from 'framer-motion';
 import styles from './BagItens.module.css';
 import { ReactComponent as Trash } from '../../assets/svg/svgCarrinho/delete.svg';
 import { ReactComponent as ExpandMore } from '../../assets/svg/svgCarrinho/expand_more.svg';
+import { NavLink } from 'react-router-dom';
 
 const BagItens = ({
   name,
@@ -26,10 +27,37 @@ const BagItens = ({
   const [codigoCorSelecionada, setCodigoCorSelecionada] = React.useState(
     itens[index].codeColor || '',
   );
+  const [sizeSelecionada, setSizeSelecionada] = React.useState(
+    sizeSelected || '',
+  );
   const [amounts, setAmounts] = React.useState(itens);
+  const [descricao, setDescricao] = React.useState(description);
+
   const [modalDelete, setModalDelete] = React.useState(false);
   const [modalColors, setModalColors] = React.useState(false);
+  const [modalSizes, setModalSizes] = React.useState(false);
+
+  const [P, setP] = React.useState(undefined);
+  const [M, setM] = React.useState(undefined);
+  const [G, setG] = React.useState(undefined);
+  const [GG, setGG] = React.useState(undefined);
+
   const url = `${api.getUri()}files/products/`;
+  const colorsIndex = colors.findIndex((cor) => cor === colorSelected);
+
+  const handleCheckColor = React.useCallback(
+    (e, i) => {
+      +stock.sizeP.amount[i] ? setP((P) => true) : setP((P) => false),
+        +stock.sizeM.amount[i] ? setM((M) => true) : setM((M) => false),
+        +stock.sizeG.amount[i] ? setG((G) => true) : setG((G) => false),
+        +stock.sizeGG.amount[i] ? setGG((GG) => true) : setGG((GG) => false);
+    },
+    [stock],
+  );
+
+  React.useEffect(() => {
+    handleCheckColor('', colorsIndex);
+  }, [handleCheckColor, colorsIndex]);
   return (
     <div className={styles.bagItem}>
       <div
@@ -37,13 +65,19 @@ const BagItens = ({
         className={styles.image}
       ></div>
       <div className={styles.infos}>
-        <h2 className={styles.name}>{name}</h2>
+        <h2 className={styles.name}>
+          <NavLink to={`/produtos/${itens[index].id}`}>
+            {name.length > 14 ? name.slice(0, 14) + '...' : name}
+          </NavLink>
+        </h2>
         <span className={styles.price}>
           {price.toLocaleString('pt-BR').includes(',')
             ? `R$ ${price.toLocaleString('pt-BR')}`
             : `R$ ${price},00`}
         </span>
-        <span className={styles.description}>{description}</span>
+        <span className={styles.description}>
+          {descricao.length > 14 ? descricao.slice(0, 27) + '...' : descricao}
+        </span>
         <div className={styles.addAmount}>
           <button
             onClick={() => {
@@ -68,45 +102,151 @@ const BagItens = ({
             -
           </button>
         </div>
-        <span
-          className={`${styles.color} ${modalColors ? styles.selectMode : ''}`}
-        >
-          <div
-            className={styles.divColorSelected}
-            onClick={() => setModalColors(!modalColors)}
+
+        <div className={styles.colorSizes}>
+          <span
+            className={`${styles.color} ${
+              modalColors ? styles.selectMode : ''
+            }`}
           >
-            <span
-              className={`${styles.corSelecionada} `}
-              style={{ backgroundColor: `${codigoCorSelecionada}` }}
-            ></span>
-            {corSelecionada}
-            {<ExpandMore />}
-          </div>
-          {modalColors && colors && (
-            <div className={styles.opcoesColors}>
-              <ul>
-                {colors.map((cor, index) => (
-                  <div key={index}>
+            <div
+              className={styles.divColorSelected}
+              onClick={() => setModalColors(!modalColors)}
+            >
+              <span
+                className={`${styles.corSelecionada} `}
+                style={{ backgroundColor: `${codigoCorSelecionada}` }}
+              ></span>
+              {corSelecionada}
+              {<ExpandMore />}
+            </div>
+            {modalColors && colors && (
+              <div className={styles.opcoesColors}>
+                <ul>
+                  {colors.map((cor, i) => (
+                    <div key={i}>
+                      <li
+                        onClick={(e) => {
+                          setCorSelecionada(cor);
+                          setCodigoCorSelecionada(codes[i]);
+                          setModalColors(false);
+                          handleCheckColor('', index);
+
+                          let quantidade = [...amounts];
+                          console.log(quantidade);
+                          quantidade[index].color = cor;
+                          quantidade[index].codeColor = codes[i];
+                          window.localStorage.setItem(
+                            'bag',
+                            JSON.stringify(quantidade),
+                          );
+                        }}
+                      >
+                        {' '}
+                        <span
+                          className={styles.corSelecionada}
+                          style={{ backgroundColor: `${codes[i]}` }}
+                        ></span>
+                        {cor}
+                      </li>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </span>
+
+          <span
+            className={`${styles.sizes} ${modalSizes ? styles.selectSize : ''}`}
+          >
+            <div
+              className={styles.divSizeSelected}
+              onClick={() => setModalSizes(!modalSizes)}
+            >
+              {sizeSelecionada}
+              {<ExpandMore />}
+            </div>
+            {modalSizes && stock && (
+              <div className={styles.opcoesSizes}>
+                <ul>
+                  {P && (
                     <li
                       onClick={() => {
-                        setCorSelecionada(cor);
-                        setCodigoCorSelecionada(codes[index]);
-                        setModalColors(false);
+                        setSizeSelecionada('P');
+                        setModalSizes(false);
+
+                        let quantidade = [...amounts];
+                        console.log(quantidade);
+                        quantidade[index].size = 'P';
+                        window.localStorage.setItem(
+                          'bag',
+                          JSON.stringify(quantidade),
+                        );
                       }}
                     >
-                      {' '}
-                      <span
-                        className={styles.corSelecionada}
-                        style={{ backgroundColor: `${codes[index]}` }}
-                      ></span>
-                      {cor}
+                      P
                     </li>
-                  </div>
-                ))}
-              </ul>
-            </div>
-          )}
-        </span>
+                  )}
+                  {M && (
+                    <li
+                      onClick={() => {
+                        setSizeSelecionada('M');
+                        setModalSizes(false);
+
+                        let quantidade = [...amounts];
+                        console.log(quantidade);
+                        quantidade[index].size = 'M';
+                        window.localStorage.setItem(
+                          'bag',
+                          JSON.stringify(quantidade),
+                        );
+                      }}
+                    >
+                      M
+                    </li>
+                  )}
+                  {G && (
+                    <li
+                      onClick={() => {
+                        setSizeSelecionada('G');
+                        setModalSizes(false);
+
+                        let quantidade = [...amounts];
+                        console.log(quantidade);
+                        quantidade[index].size = 'G';
+                        window.localStorage.setItem(
+                          'bag',
+                          JSON.stringify(quantidade),
+                        );
+                      }}
+                    >
+                      G
+                    </li>
+                  )}
+                  {GG && (
+                    <li
+                      onClick={() => {
+                        setSizeSelecionada('GG');
+                        setModalSizes(false);
+
+                        let quantidade = [...amounts];
+                        console.log(quantidade);
+                        quantidade[index].size = 'GG';
+                        window.localStorage.setItem(
+                          'bag',
+                          JSON.stringify(quantidade),
+                        );
+                      }}
+                    >
+                      GG
+                    </li>
+                  )}
+                </ul>
+              </div>
+            )}
+          </span>
+        </div>
+
         <span className={styles.lixeira} onClick={() => setModalDelete(true)}>
           {<Trash />}
         </span>
@@ -121,21 +261,21 @@ const BagItens = ({
           }}
         >
           <div className={styles.gridDelete}>
-            <h4>Deseja remover o item do carrinho?</h4>
+            <h4>Tem certeza de excluir esse item?</h4>
             <div className={styles.botoes}>
+              <button onClick={() => setModalDelete(false)}>Cancelar</button>
+
               <button
                 onClick={(e) => {
                   const newItens = [...itens];
                   newItens.splice(index, 1);
                   window.localStorage.setItem('bag', JSON.stringify(newItens));
                   setModalDelete(false);
-
                   e.target.parentElement.parentElement.parentElement.parentElement.remove();
                 }}
               >
-                Sim
+                Excluir
               </button>
-              <button onClick={() => setModalDelete(false)}>Não</button>
             </div>
           </div>
         </div>
